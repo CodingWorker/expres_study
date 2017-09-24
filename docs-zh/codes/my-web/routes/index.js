67ed20b7f93f50af4f2077d117ff5e4696b9c83b
CodingWorker <dy10010@sina.cn>
2017-09-24 19:29:53 +0800

    add

--- docs-zh/codes/my-web/routes/index.js
+++ docs-zh/codes/my-web/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello',(req,res,next)=>{
+  res.render('hello',{
+    title:'hello express'
+  });
+});
+
+router.post('/login',(req,res,next)=>{
+  console.log(req);
+  res.end();
+});
+
 module.exports = router;
